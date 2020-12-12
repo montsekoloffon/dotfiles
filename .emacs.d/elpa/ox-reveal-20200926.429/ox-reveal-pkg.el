e49677a692e3dde50a84b2611ef148b18451cb41
Montserrat Koloffon <montserrat@Montserrats-MBP.lan>
2020-12-12 16:17:03 +0100

    removed temporarily emacs

--- .emacs.d/elpa/ox-reveal-20200926.429/ox-reveal-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/montserrat/.emacs.d/elpa/ox-reveal-20200926.429/ox-reveal.el  -*- no-byte-compile: t -*-
-(define-package "ox-reveal" "20200926.429" "reveal.js Presentation Back-End for Org Export Engine" '((org "8.3")) :commit "79b01ae85bccf3622dc79d17d3eceebfa9a81636" :keywords '("outlines" "hypermedia" "slideshow" "presentation") :authors '(("Yujie Wen <yjwen.ty at gmail dot com>")) :maintainer '("Yujie Wen <yjwen.ty at gmail dot com>"))
